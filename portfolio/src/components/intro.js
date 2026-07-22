6889ecdc57305b2efa3782e75f227f450ccf0241
xavimagine <sr.xavier5@gmail.com>
2026-07-22 20:53:36 +0200

    chore: :construction: Implementando Aboutme

---
+++ portfolio/src/components/intro.js
@@ -0,0 +1,34 @@
+// Descargar cv
+const boton = document.getElementById("Dcv");
+boton?.addEventListener("click", () => {
+    const enlace = document.createElement("a");
+    enlace.href = "/cv/Desarrollador_Web_CV_Javier_Diaz_Rayo.pdf";
+    enlace.download = "Desarrollador_Web_CV_Javier_Diaz_Rayo.pdf";
+    enlace.click();
+});
+
+// Efecto Máquina de Escribir
+const contenedor = document.getElementById("texto-efecto");
+const textoInt = contenedor?.dataset.texto || "";
+
+let i = 0;
+const velocidad = 22;
+
+if (contenedor && textoInt) {
+    contenedor.textContent = "";
+
+    const cursor = document.createElement("span");
+    cursor.className = "cursor";
+    contenedor.appendChild(cursor);
+
+    function escribir() {
+        if (i < textoInt.length) {
+            cursor.before(textoInt.charAt(i));
+            i++;
+            setTimeout(escribir, velocidad);
+        }
+    }
+    if (textoInt) {
+        escribir();
+    }
+}
